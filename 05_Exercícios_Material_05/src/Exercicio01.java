95c5633b2007193b04808c04523a8789e012d9a1
lkd8 <164064194+lkd8@users.noreply.github.com>
2024-03-19 14:47:08 -0300

    Add files via upload

---
+++ 05_Exercícios_Material_05/src/Exercicio01.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Exercicio01 {
+	public static void main(String[] args) {
+		
+		// variáveis
+		Scanner in = new Scanner(System.in);
+		double base, altura, area, perimetro;
+		
+		// entrada de dados
+		System.out.print("Valor da base ");
+		base = in.nextDouble();
+		System.out.print("Valor da altura ");
+		altura = in.nextDouble();
+		
+		// processamento de dados
+		area = base * altura;
+		perimetro = 2 * (base + altura);
+		
+		// saída de dados
+		System.out.println();
+		System.out.println("Área do retângulo " + area);
+		System.out.println("Perímetro do retângulo " + perimetro);
+
+	}
+}
---
+++ 05_Exercícios_Material_05/src/Exercicio05.java
@@ -0,0 +1,32 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Exercicio05 {
+
+	public static void main(String[] args) {
+		
+		// variáveis
+		Scanner in = new Scanner(System.in);
+		double valorhora, horatrab, desconto, bruto, liquido;
+		
+		DecimalFormat mascara = new DecimalFormat("0.00");
+		
+		// entrada de dados
+		System.out.print("Valor da hora-aula ");
+		valorhora = in.nextDouble();
+		System.out.print("Horas trabalhadas ");
+		horatrab = in.nextDouble();
+		System.out.print("Desconto INSS ");
+		desconto = in.nextDouble();
+		
+		// processamento de dados
+		bruto = valorhora * horatrab;
+		liquido = bruto - desconto;
+		
+		// saída de dados
+		System.out.println();
+		System.out.println("Salário liquido " + mascara.format(liquido));
+
+	}
+
+}
